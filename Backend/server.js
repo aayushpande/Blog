82f0eb3456772b3a9e3d509f3ec1ff8480adee24
aayushpande <aayushpandey472@gmail.com>
2024-07-02 01:28:21 +0530

    second-commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,23 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // Output directory for the build
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Output directory for the build
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
   },
 });
